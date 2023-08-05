2cbeb83bb98896edccb39c9f5fb8bd038ba0a0ad
violet666666 <setanhantu11111@gmail.com>
2023-08-06 03:38:20 +0700

    first innit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Sidebar from './component/sidebar/Sidebar';
+import Home from './component/home/Home';
+import About from './component/about/About';
+import Services from './component/services/Services';
+import Resume from './component/resume/Resume';
+import Portfolio from './component/portfolio/Portfolio';
+import Pricing from './component/pricing/Pricing';
+import Testimonials from './component/testimonials/Testimonials';
+import Blog from './component/blog/Blog';
+import Contact from './component/contact/Contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<>
+<Sidebar />
+<main className='main'>
+  <Home />
+  <About />
+  <Services />
+  <Resume />
+  <Portfolio />
+  <Pricing />
+  <Testimonials />
+  <Blog />
+  <Contact />
+</main>
+</>  
+)
 }
 
 export default App;
---
+++ src/component/resume/Resume.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './resume.css'
+const Resume = () => {
+  return (
+    <div>Resume</div>
+  )
+}
+
+export default Resume
