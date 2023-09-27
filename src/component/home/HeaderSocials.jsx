a2e35e31a8a424dee44f04e1bac2ac6fc35767e1
violet666666 <setanhantu11111@gmail.com>
2023-09-27 19:26:06 +0700

    add headersocials, scrolldown, and shapes to home component and edit some css

---
+++ src/component/home/HeaderSocials.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+        <a href="https://www.instagram.com/" className="home__social-link">
+        <i class="fa-brands fa-instagram"></i>
+        </a>
+
+        <a href="https://www.twitter.com/" className="home__social-link">
+        <i class="fa-brands fa-twitter"></i>
+        </a>
+
+        <a href="https://www.behance.com/" className="home__social-link">
+        <i class="fa-brands fa-behance"></i>
+        </a>
+
+        <a href="https://www.dribble.com/" className="home__social-link">
+        <i class="fa-brands fa-dribbble"></i>
+        </a>
+
+        <a href="https://www.pinterest.com/" className="home__social-link">
+        <i class="fa-brands fa-pinterest"></i>
+        </a>
+    </div>
+      )
+}
+
+export default HeaderSocials
--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './home.css';
-
+import Me from '../../assets/avatar-1.svg';
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
     return (
-        <div>Home</div>
-    )
+        <section className="home container" id="home">
+            <div className="intro">
+                <img src={Me} alt="" className="home__img" />
+                <h1 className="home__name">Tegar Alfalakh Thamrin</h1>
+                <span className="home__education">I'm Junior Front-End Developer</span>
+                <HeaderSocials/>
+                <a href="#contact" className="btn">Hire Me</a>  
+                <ScrollDown/>
+            </div>
+        </section>
+    );
 }
 
 export default Home;
